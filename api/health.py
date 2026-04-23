6ff216b9483ad293e1f85915ec796c8db01c8c08
JDM-Github <jdmaster888@gmail.com>
2026-04-23 20:12:58 +0800

    Changes to be committed: 	deleted:    api/__init__.py 	deleted:    api/example.py 	deleted:    api/health.py

--- api/health.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from app.utils.responses import success
-
-health_bp = Blueprint("health", __name__)
-
-@health_bp.route("/health", methods=["GET"])
-def health():
-    return success({"status": "ok"}, "Service is running")
